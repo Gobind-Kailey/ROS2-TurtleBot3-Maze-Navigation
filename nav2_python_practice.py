f41b19496d284f26a1e667282c85c6053b6410fe
Gobind Kailey <gobind2975@gmail.com>
2025-09-20 11:28:51 -0400

    Creating navigation goal for robot

--- nav2_python_practice.py
+++ nav2_python_practice.py
@@ -34,6 +34,30 @@ def main():
     nav.waitUntilNav2Active()
 
 
+    # Send Nav2 goal 
+    # 1.57 = Pi/2 -- so 90 degrees CCW
+    q_x, q_y, q_z, q_w = tf_transformations.quaternion_from_euler(0.0, 0.0, 1.57)
+
+    goal_pose = PoseStamped()
+    goal_pose.header.frame_id = 'map'
+    goal_pose.header.stamp = nav.get_clock().now().to_msg()
+    goal_pose.pose.position.x = 3.5
+    goal_pose.pose.position.y = 1.0
+    goal_pose.pose.position.z = 0.0
+
+
+    goal_pose.pose.orientation.x = q_x
+    goal_pose.pose.orientation.y = q_y
+    goal_pose.pose.orientation.z = q_z
+    goal_pose.pose.orientation.w = q_w 
+
+    nav.goToPose(goal_pose) 
+
+# dummy
+    while not nav.isTaskComplete():
+        feedback = nav.getFeedback()
+        # print(feedback)
+
     # Shutdown 
 
 
